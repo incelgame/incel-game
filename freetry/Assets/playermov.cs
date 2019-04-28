3b2d3623a13a9ce24eace333fa949248accbdeda
incelgamec <50073262+incelgame@users.noreply.github.com>
2019-04-28 16:33:59 +0100

    first upload

---
+++ freetry/Assets/playermov.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermov : MonoBehaviour {
+
+    public int speed;
+    public float animint;
+    public Sprite[] sprites;
+    public Sprite stop;
+    public bool stopmoving;
+
+    private Vector2 input;
+    private float nextframe;
+    private int index;
+    private bool speed0;
+    
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!stopmoving)
+        {
+            speed0 = true;
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            if (speed0)
+            {
+                input.x = 0;
+                input.y = 0;
+                speed0 = false;
+            }
+        }
+
+
+        if (input.x > 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
+        }
+        if (input.x < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
+        }
+
+        if(Time.time > nextframe & input != new Vector2(0,0))
+        {
+            nextframe = Time.time + animint;
+            GetComponent<SpriteRenderer>().sprite = sprites[index];
+            index += 1;
+            if(index >= sprites.Length)
+            {
+                index = 0;
+            }
+        }
+        if(input == new Vector2(0, 0))
+        {
+            GetComponent<SpriteRenderer>().sprite = stop;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = input * speed;
+
+    }
+}
---
+++ freetry/Assets/talkpoint.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class talkpoint : MonoBehaviour {
+
+    public string[] alltext;
+    public Sprite[] allimag;
+    public Sprite incel;
+    public Sprite itcel;
+
+    public GameObject panel;
+    public Text text;
+    public Image imag;
+
+    
+    private bool start;
+    private int index;
+    private GameObject player;
+    private AudioSource aud;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        player = collision.gameObject;
+        panel.SetActive(true);
+        collision.gameObject.GetComponent<playermov>().stopmoving = true;
+        start = true;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        aud = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (start)
+        {
+            imag.sprite = allimag[index];
+            text.text = alltext[index];
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                index += 1;
+                aud.Play();
+            }
+            
+            if(index > alltext.Length-1)
+            {
+                index = 0;
+                start = false;
+                panel.SetActive(false);
+                player.GetComponent<playermov>().stopmoving = false;
+                gameObject.SetActive(false);
+
+            }
+
+        }
+	}
+}
